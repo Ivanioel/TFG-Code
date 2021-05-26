c113ddafcabfddfe6b40579126fb9f0ea63a3299
Tu Nombre <you@example.com>
2021-05-26 23:03:04 +0200

    Cambios en nombres

--- codigo/ridgePCA.py
+++ codigo/ridgePCA.py
@@ -15,10 +15,13 @@ fullDataset = pd.read_csv('../datasets/data_target_stv_2017.csv', index_col=0, p
 x_col, target_col = fullDataset.columns[:-1], fullDataset.columns[-1]
 data, target = fullDataset[x_col], fullDataset[target_col]
 
+# Creación de PCA sin parámetros para luego hiperparametrizar
+pca = PCA()
 # Creación del modelo de regresion final
 reg = Ridge(max_iter=10000)
 # Pipeline
 regr_base = Pipeline(steps=[('std_sc', StandardScaler()),
+                            ('pca', pca),
 				            ('reg', reg)])
 
 # Estandarizamos el target también
@@ -26,9 +29,11 @@ y_transformer = StandardScaler()
 inner_estimator = TransformedTargetRegressor(regressor=regr_base, transformer=y_transformer)
 
 # Parametros a hiperparametrizar
-l_alpha = [10.**k for k in range(1, 4)]
+l_alpha = [10.**k for k in range(-3, 5)]
+n_components = list(range(50, 301, 25))
 param_grid = {
     'regressor__reg__alpha': l_alpha,
+    'regressor__pca__n_components': n_components,
 }
 # Numero de splits para Cross Validation
 n_splits = 6
@@ -39,9 +44,8 @@ cv_estimator = GridSearchCV(estimator=inner_estimator,
                             cv=kf, 
                             scoring='neg_mean_absolute_error', 
                             return_train_score=True, 
-                            n_jobs=3, 
-                            verbose=1,
-                            refit=True)
+                            n_jobs=5, 
+                            verbose=1)
 
 # Fit datos 
 _ = cv_estimator.fit(data.values, target.values)
@@ -49,7 +53,7 @@ print(f"Mejor estimador: {cv_estimator.best_estimator_}")
 print(f"Mejor puntuacion - mejor estimador: {cv_estimator.best_score_}")
 # Guardar datos en fichero
 path = '../estimators/'
-filename = f'cv_estimator__ridge_no_PCA'
+filename = f'cv_estimator__ridge_{n_components[0]}_{n_components[-1]}_{n_components[1]-n_components[0]}'
 fullPath = path + filename
 dump(cv_estimator, fullPath)
 
